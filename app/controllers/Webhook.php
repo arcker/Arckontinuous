6d00646041cc064d451de281f58dc8b698411c46
arcker <arcker@gmail.com>
2023-04-15 16:53:48 +0200

    add Webhooks

--- app/controllers/Webhook.php
+++ app/controllers/Webhook.php
@@ -4,12 +4,29 @@ namespace controllers;
 /**
  * Controller Webhook
  */
+use Ubiquity\core\postinstall\Display;
+use Ubiquity\log\Logger;
+use Ubiquity\themes\ThemesManager;
+use Ubiquity\views\engine\latte\Latte;
+
 
 class Webhook extends ControllerBase {
 
 	public function index(){
-	    //TODO index action implementation
+		$latte = new Latte;
+		// $latte->
+		// $latte->setTempDirectory(__DIR__ . '/temp');
+		// $latte->setautoRefresh();
+		// $options = $k8s->getOptions();
+		// $kubeConfig = $options->getKubeConfigContext();
+		// echo ($kubeConfig->getServer());
+
+		// $parameters['kubeconfig'] = $kubeConfig->getServer();
+	
+		$parameters['items'] = ['one', 'two', 'three'];
+		$latte->render('Webhooks/index.latte', $parameters);
 		
+
 	}
 }
 
